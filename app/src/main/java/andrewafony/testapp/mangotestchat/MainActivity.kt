2ca73ae34ef6bfd5f2ac7fe8a05f5802226c8495
Andrew <andrew.afony@yandex.ru>
2024-12-03 14:49:05 +0300

    initial adjustments

--- app/src/main/java/andrewafony/testapp/mangotestchat/MainActivity.kt
+++ app/src/main/java/andrewafony/testapp/mangotestchat/MainActivity.kt
@@ -1,4 +1,4 @@
-package andrewafony.testapp.mangotestchat.presentation
+package andrewafony.testapp.mangotestchat
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import andrewafony.testapp.mangotestchat.theme.theme.MangoTestChatTheme
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +20,14 @@ class MainActivity : ComponentActivity() {
             MangoTestChatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+                    ChatApp()
                 }
             }
         }
     }
+}
+
+@Composable
+fun ChatApp() {
+
 }
