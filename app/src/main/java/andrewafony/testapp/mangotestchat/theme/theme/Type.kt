36a8e8f8ce91c42defa55c2f808c51b583a95bd1
Andrew <andrew.afony@yandex.ru>
2024-12-03 14:31:53 +0300

    Initial commit

---
+++ app/src/main/java/andrewafony/testapp/mangotestchat/theme/theme/Type.kt
@@ -0,0 +1,69 @@
+package andrewafony.testapp.mangotestchat.theme.theme
+
+import andrewafony.testapp.mangotestchat.R
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+private val robotoFontFamily = FontFamily(
+    Font(R.font.roboto_thin, weight = FontWeight.W100),
+    Font(R.font.roboto_light, weight = FontWeight.W300),
+    Font(R.font.roboto_regular, weight = FontWeight.W400),
+    Font(R.font.roboto_medium, weight = FontWeight.W500),
+    Font(R.font.roboto_bold, weight = FontWeight.W600),
+    Font(R.font.roboto_black, weight = FontWeight.W900),
+)
+
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.W300,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+    ),
+    titleLarge = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+)
