640785dac1b89bcc3348a96b594d3347b945e468
Andrew <andrew.afony@yandex.ru>
2024-12-16 15:02:57 +0300

    add splash screen update MainActivity.kt

--- app/src/main/java/andrewafony/testapp/mangotestchat/MainActivity.kt
+++ app/src/main/java/andrewafony/testapp/mangotestchat/MainActivity.kt
@@ -4,11 +4,17 @@ import andrewafony.testapp.designsystem.theme.MangoTestChatTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.compose.KoinContext
@@ -20,9 +26,29 @@ class MainActivity : ComponentActivity() {
     private val featureProvider: FeatureDestinationProvider by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
 
-        val isLogged = viewModel.isLogged()
+        var mainState by mutableStateOf(MainActivityState())
+        var isLoggedIn: Boolean? by mutableStateOf(null)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                viewModel.state.collectLatest {
+                    mainState = it
+                }
+            }
+        }
+
+        splashScreen.setKeepOnScreenCondition {
+            when (mainState.screenState) {
+                is MainActivityScreenState.Loading -> true
+                is MainActivityScreenState.Success -> {
+                    isLoggedIn = mainState.isLogged
+                    false
+                }
+            }
+        }
 
         setContent {
 
@@ -31,17 +57,18 @@ class MainActivity : ComponentActivity() {
             val backStack by navController.currentBackStackEntryAsState()
             val currentRoute = backStack?.destination?.route
 
-            KoinContext {
-                MangoTestChatTheme {
-                    MainScreen(
-                        navController = navController,
-                        currentRoute = currentRoute,
-                        featureProvider = featureProvider,
-                        isLogged = isLogged
-                    )
+            isLoggedIn?.let {
+                KoinContext {
+                    MangoTestChatTheme {
+                        MainScreen(
+                            navController = navController,
+                            currentRoute = currentRoute,
+                            featureProvider = featureProvider,
+                            isLogged = it
+                        )
+                    }
                 }
             }
         }
     }
 }
-
--- app/src/main/java/andrewafony/testapp/mangotestchat/MainViewModel.kt
+++ app/src/main/java/andrewafony/testapp/mangotestchat/MainViewModel.kt
@@ -2,15 +2,39 @@ package andrewafony.testapp.mangotestchat
 
 import andrewafony.testapp.data.utils.TokenManager
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 
 class MainViewModel(
     private val tokenManager: TokenManager
 ) : ViewModel() {
 
-    fun isLogged(): Boolean = runBlocking {
-        tokenManager.accessToken.first().isNotBlank() &&
-        tokenManager.refreshToken.first().isNotBlank()
+    val state = combine(
+        tokenManager.accessToken, tokenManager.refreshToken
+    ) { access, refresh ->
+        MainActivityState(
+            screenState = MainActivityScreenState.Success,
+            isLogged = access.isNotBlank() && refresh.isNotBlank()
+        )
     }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = MainActivityState()
+        )
+}
+
+data class MainActivityState(
+    val screenState: MainActivityScreenState = MainActivityScreenState.Loading,
+    val isLogged: Boolean = false
+)
+
+sealed interface MainActivityScreenState {
+
+    data object Loading : MainActivityScreenState
+
+    data object Success : MainActivityScreenState
+
 }
